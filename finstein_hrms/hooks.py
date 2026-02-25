67059c08d00937eeae4fc04e065291a3b3a5583f
bharathirajafins-pixel <bharathirajafins@gmail.com>
2026-02-25 12:44:04 +0530

    Update hooks.py

--- finstein_hrms/hooks.py
+++ finstein_hrms/hooks.py
@@ -277,7 +277,7 @@ doc_events = {
     "Leave Application": {
         "validate": "finstein_hrms.server_script.leave_validation.validate_leave_dates"
 }
-
+}
 doc_events = {
     "Attendance Request": {
         "validate": "finstein_hrms.server_script.attendances_request.validate"
